f3352514538ef3b6264caaf299ad5eaa0e4ef11c
lhdmir <lhdmir@naver.com>
2025-06-25 23:51:51 +0900

    fix: 의존성 변경

--- src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -3,8 +3,6 @@ package com.example.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
-
 @SpringBootApplication
 public class GatewayServiceApplication {
 
